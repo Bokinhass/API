10791d53458a10729fa368ecb8b5b19ef47de901
Maciel <pedrohenrique_maciel@hotmail.com>
2023-07-15 12:40:39 -0300

    fix hisotry check-ins and 20 itens for page

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,31 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '../repositories/check-ins-repository'
+
+interface FetchUserCheckInHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInHistoryUseCaseCase {
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+  ) { }
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInHistoryUseCaseRequest): Promise<FetchUserCheckInHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId, 
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
