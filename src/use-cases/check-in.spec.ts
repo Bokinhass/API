325d3b46ff8ac1ba5b2592b16f1b4e0bb4cfcf05
Maciel <pedrohenrique_maciel@hotmail.com>
2023-07-15 10:10:41 -0300

    fix all bugs and create gym check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,65 +1,95 @@
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
-import { expect, describe, it, vi, afterEach } from 'vitest'
+import { expect, describe, it, vi, beforeEach, afterEach } from 'vitest'
 import { CheckInsUseCase } from './check-in'
-import { beforeEach } from 'node:test'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime'
 
 let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGymsRepository
 let sut: CheckInsUseCase
 
-describe('Register Use Case', () => {
+describe('Check-in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
-    sut = new CheckInsUseCase(checkInsRepository)
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CheckInsUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: 'gym-01',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-21.6513858),
+      longitude: new Decimal(-41.7504137),
+    })
 
     vi.useFakeTimers()
   })
-
   afterEach(() => {
     vi.useRealTimers()
   })
-
   it('should be able to check in', async () => {
-    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
-
     const { checkIn } = await sut.execute({
       gymId: 'gym-01',
       userId: 'user-01',
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    })
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    })
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
+    })
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401091,
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
   })
 
-  it('should not be able to check in twice on the same day', async () => {
-    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
-
-    await sut.execute({
-      gymId: 'gym-01',
-      userId: 'user-01',
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.items.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-27.2092052),
+      longitude: new Decimal(-49.6401091),
     })
 
-    await expect(() => {
+    await expect(() =>
       sut.execute({
-        gymId: 'gym-01',
+        gymId: 'gym-02',
         userId: 'user-01',
-      })
-    }).rejects.toBeInstanceOf(Error)
-  })
-
-  it('should be able to check in twice but in different days', async () => {
-    vi.setSystemTime(new Date(2023, 0, 20, 8, 0, 0))
-
-    await sut.execute({
-      gymId: 'gym-01',
-      userId: 'user-01',
-    })
-
-    vi.setSystemTime(new Date(2023, 0, 21, 8, 0, 0))
-
-    await expect(() => {
-      sut.execute({
-        gymId: 'gym-01',
-        userId: 'user-01',
-      })
-    }).resolves.toBeTruthy()
+        userLatitude: -27.2092052,
+        userLongitude: -49.6401091,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
